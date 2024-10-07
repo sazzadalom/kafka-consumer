b2ffc47c5418a2f8905db218ef0ff45a5aef5d3f
Sazzad Alom <sazzad.alom@mosambee.in>
2024-10-07 14:30:44 +0530

    commit

--- src/main/java/com/alom/ConsumerApplication.java
+++ src/main/java/com/alom/ConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlomKafkaConsumerApplication {
+public class ConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AlomKafkaConsumerApplication.class, args);
+		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
 }
